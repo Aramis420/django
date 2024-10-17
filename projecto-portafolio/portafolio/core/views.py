e6290975d534eec7cd49f1d93114c34bb50ec20c
Aramis <aramisdavidarenasgarrido@gmail.com>
2024-10-17 01:46:07 -0300

    Se agrego una app llamada core con 4 htmls, como tambien se configuro la zona horaria he el idioma en el archivo settings.py.

--- projecto-portafolio/portafolio/core/views.py
+++ projecto-portafolio/portafolio/core/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 
 # Create your views here.
+html_cabecera="""
+<h1>Mi Portafolio Personal</h1>
+<ul>
+    <li> <a href="/">Inicio</a> </li>
+    <li> <a href="/about-me/">Acerca de</a> </li>
+    <li> <a href="/portfolio/">Portafolio</a> </li>
+    <li> <a href="/contact/">Contacto</a> </li>
+</ul>
+"""
+
+def home(request):
+    return render(request,'core/home.html')
+def about(request):
+    return render(request,'core/about.html')
+def portfolio(request):
+    return render(request,'core/portfolio.html')
+def contact(request):
+    return render(request,'core/contact.html')
